f4db64402d5c4d7657a21fafcde46331a9fcafb2
Luis Felipe Rodrigues Freire <20231011110028@ifrn.local>
2025-04-22 13:47:31 -0300

    cobra cobra cobra

--- aula02/Loja/loja/models/Produto.py
+++ aula02/Loja/loja/models/Produto.py
@@ -13,5 +13,7 @@ related_name='categoria', on_delete=models.SET_NULL)
 related_name='fabricante', on_delete=models.SET_NULL)
     criado_em = models.DateTimeField(auto_now_add=True)
     alterado_em = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True, blank=True)
+
     def __str__(self):
         return '{}'.format(self.Produto)
