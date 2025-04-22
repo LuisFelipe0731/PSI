268bb24b12a6b0813c95be1770b789f237b7b80a
Luis Felipe Rodrigues Freire <20231011110028@ifrn.local>
2025-04-22 13:31:20 -0300

    nirvana

--- aula02/Loja/loja/admin.py
+++ aula02/Loja/loja/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin #isso já vai estar existindo no arquivo
-
-from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+from django.contrib import admin
+# Register your models here.
+from .models import *
+admin.site.register(Fabricante)
+admin.site.register(Categoria)
+admin.site.register(Produto)
