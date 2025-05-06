aae3f948fa055aad487e963b941e234d191a642e
Luis Felipe Rodrigues Freire <20231011110028@ifrn.local>
2025-05-06 14:07:51 -0300

    xuxa para baixinhos

--- aula02/Loja/loja/admin.py
+++ aula02/Loja/loja/admin.py
@@ -10,6 +10,9 @@ class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
     'preco', 'categoria',)
     empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao',
+    'msgPromocao', 'preco', 'categoria',)
+    search_fields = ('Produto',)
 
 admin.site.register(Fabricante,FabricanteAdmin)
 admin.site.register(Categoria)
