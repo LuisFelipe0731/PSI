d2cd670636d04388a53a1dcd61e27011ec706735
Luis Felipe Rodrigues Freire <20231011110028@ifrn.local>
2025-06-10 13:35:33 -0300

    queen

--- aula02/Loja/loja/views/ProdutoView.py
+++ aula02/Loja/loja/views/ProdutoView.py
@@ -1,8 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render # Retire from django.http import HttpResponse
 from loja.models import Produto
-
 from datetime import timedelta, datetime
 from django.utils import timezone
+
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)
 
 def list_produto_view(request, id=None):
     produto = request.GET.get("produto")
@@ -31,6 +39,8 @@ def list_produto_view(request, id=None):
         produtos = produtos.filter(fabricante__Fabricante=fabricante)
     
     if id is not None:
-        produtos = produtos.filter(id=id)
-    print(produtos)
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
+    context = { 'produtos': produtos }
+    return render(request, template_name='produto/produto.html', context=context, status=200)
