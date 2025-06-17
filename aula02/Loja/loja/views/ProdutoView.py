e9cdc281f0e6460d5f9ecdfc954c01e63829d3bf
Luis Felipe Rodrigues Freire <20231011110028@ifrn.local>
2025-06-17 13:53:47 -0300

    good morning

--- aula02/Loja/loja/views/ProdutoView.py
+++ aula02/Loja/loja/views/ProdutoView.py
@@ -52,6 +52,9 @@ def edit_produto_postback(request, id=None):
         promocao = request.POST.get("promocao")
         msgPromocao = request.POST.get("msgPromocao")
         
+        categoria = request.POST.get("CategoriaFk")
+        fabricante = request.POST.get("FabricanteFk")
+
         print("postback")
         print(id)
         print(produto)
@@ -63,6 +66,8 @@ def edit_produto_postback(request, id=None):
             obj_produto.Produto = produto
             obj_produto.destaque = (destaque is not None)
             obj_produto.promocao = (promocao is not None)
+            obj_produto.fabricante = Fabricante.objects.filter(id=fabricante).first()
+            obj_produto.categoria = Categoria.objects.filter(id=categoria).first()
             
             if msgPromocao is not None:
                 obj_produto.msgPromocao = msgPromocao
