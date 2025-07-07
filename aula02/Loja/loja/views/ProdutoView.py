422bb3274bf14c94ff50e7ed145d0e07f22a98d3
Luis Felipe Rodrigues Freire <20231011110028@ifrn.local>
2025-07-07 15:44:35 -0300

    oi

--- aula02/Loja/loja/views/ProdutoView.py
+++ aula02/Loja/loja/views/ProdutoView.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect # Retire from django.http import HttpResponse
 from loja.models import Produto, Fabricante, Categoria
 from datetime import timedelta, datetime
@@ -24,11 +25,16 @@ def delete_produto_postback(request, id=None):
 def details_produto_view(request, id=None):
     # Processa o evento GET gerado pela action
     produtos = Produto.objects.all()
+    
+    Fabricantes = Fabricante.objects.all()
+    Categorias = Categoria.objects.all()
+
     if id is not None:
         produtos = produtos.filter(id=id)
     produto = produtos.first()
     print(produto)
-    context = {'produto': produto}
+    context = {'produto': produto, 'fabricantes' : Fabricantes, 'categorias' : Categorias}
+
     return render(request, template_name='produto/produto-details.html', context=context, status=200)
 
 #deletar
@@ -89,6 +95,7 @@ def edit_produto_view(request, id=None):
         produtos = produtos.filter(id=id)
     produto = produtos.first()
     print(produto)
+    
     Fabricantes = Fabricante.objects.all()
     Categorias = Categoria.objects.all()
     context = {'produto': produto, 'fabricantes' : Fabricantes, 'categorias' : Categorias}
@@ -130,7 +137,10 @@ def list_produto_view(request, id=None):
 
 #criar produto
 def create_produto_view(request, id=None):
+    
+    
     if request.method == 'POST':
+        
         produto = request.POST.get("Produto")
         destaque = request.POST.get("destaque")
         promocao = request.POST.get("promocao")
@@ -139,6 +149,9 @@ def create_produto_view(request, id=None):
         categoria = request.POST.get("CategoriaFk")
         fabricante = request.POST.get("FabricanteFk")
         image = request.POST.get("image")
+        Fabricantes = Fabricante.objects.all()
+        Categorias = Categoria.objects.all()
+        
 
         print("postback-create")
         print(produto)
@@ -146,24 +159,29 @@ def create_produto_view(request, id=None):
         print(promocao)
         print(msgPromocao)
         print(preco)
-        print(categoria)
-        print(fabricante)
         print(image)
+        
         try:
+
             obj_produto = Produto()
             obj_produto.Produto = produto
             obj_produto.destaque = (destaque is not None)
             obj_produto.promocao = (promocao is not None)
-            obj_produto.fabricante = Fabricante.objects.filter(id=fabricante).first()
-            obj_produto.categoria = Categoria.objects.filter(id=categoria).first()
+            
             if msgPromocao is not None:
                 obj_produto.msgPromocao = msgPromocao
             obj_produto.preco = 0
+            
             if (preco is not None) and ( preco != ""):
                 obj_produto.preco = preco
+            
             obj_produto.criado_em = timezone.now()
             obj_produto.alterado_em = obj_produto.criado_em
-        # Se for anexado arquivo, salva na pasta e guarda nome no objeto
+            
+            obj_produto.fabricante = Fabricante.objects.filter(id=fabricante).first()
+            obj_produto.categoria = Categoria.objects.filter(id=categoria).first()
+            
+            # Se for anexado arquivo, salva na pasta e guarda nome no objeto
             if request.FILES is not None:
                 num_files = len(request.FILES.getlist('image'))
                 if num_files > 0:
@@ -180,5 +198,4 @@ def create_produto_view(request, id=None):
         except Exception as e:
             print("Erro inserindo produto: %s" % e)
         return redirect("/produto")
-
-    return render(request, template_name='produto/produto-create.html',status=200)
+    return render(request, template_name='produto/produto-create.html', status=200)
