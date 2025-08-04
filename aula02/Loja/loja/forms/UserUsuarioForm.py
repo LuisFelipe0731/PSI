3b26d3f8714005ccb38ed299eed028c5c5e62f61
Luis Felipe Rodrigues Freire <20231011110028@ifrn.local>
2025-08-04 15:15:59 -0300

    sucodelaranja

--- aula02/Loja/loja/forms/UserUsuarioForm.py
+++ aula02/Loja/loja/forms/UserUsuarioForm.py
@@ -4,7 +4,7 @@ from loja.models.Usuario import Usuario
 from django.contrib.auth.models import User
 
 class UserUsuarioForm(ModelForm):
-    
+
     def __init__(self, *args, **kwargs):
         super(UserUsuarioForm, self).__init__(*args, **kwargs)
         if self.instance and self.instance.perfil != 1:
@@ -20,3 +20,14 @@ class UserUsuarioForm(ModelForm):
         # Usamos exclude para excluir campos específicos do sistema
         # exclude = ['']
         widgets = {'user': forms.HiddenInput(), 'perfil': forms.Select(attrs={'class': "form-control"}), 'aniversario': forms.DateInput(attrs={'class': "form-control", "type": "date"})}
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+        'username': forms.TextInput(attrs={'class': "form-control"}),
+        'email': forms.EmailInput(attrs={'class': "form-control"}),
+        'first_name': forms.TextInput(attrs={'class': "form-control"}),
+        'last_name': forms.TextInput(attrs={'class': "form-control"})
+        }
